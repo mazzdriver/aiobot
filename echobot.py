0b37d5e2e9a001e721ad1f1c6870ef57dccd165e
mazzdriver <oleg@massdriver95.ru>
2023-02-01 11:54:23 +0300

    final lesson about echobot

---
+++ echobot.py
@@ -0,0 +1,58 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, ContentType
+
+API_TOKEN: str = '2101241794:AAESTzO7C0hgAK8J2PyK-LwnmGq-5Oe9qJw'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+
+# этот хэндлер будет срабатывать на отправку боту гифок
+async def send_gif_echo(message: Message):
+    await message.reply_animation(message.animation.file_id)
+
+
+# Этот хэндлер будет срабатывать на отправку боту голосовых сообщений
+async def send_voice_echo(message: Message):
+    await message.reply_voice(message.voice.file_id)
+
+
+# этот хэндлер будет срабатывать на отправку боту стикеров
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+# Регистрируем хэндлеры
+dp.message.register(process_start_command, Command(commands=["start"]))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_gif_echo, F.content_type == ContentType.ANIMATION)
+dp.message.register(send_voice_echo, F.content_type == ContentType.VOICE)
+dp.message.register(send_sticker_echo, F.content_type == ContentType.STICKER)
+dp.message.register(send_echo)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
